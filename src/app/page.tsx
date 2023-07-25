66722ec6613006aff0d5036a211f57e9b900925d
davique0 <davique@gmail.com>
2023-07-25 11:15:26 -0700

    cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,6 @@ export default function Home() {
             <ul className="flex items-center">
               <li className="cursor-pointer" onClick={() => setDarkMode(!darkMode)}>
                 {!darkMode ? <BsFillMoonStarsFill /> : <BsFillBrightnessHighFill />}
-                {/* <BsFillMoonStarsFill /> */}
               </li>
               <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="https://resume.creddle.io/resume/9gy20sjmykz">Resume</a></li>
             </ul>
